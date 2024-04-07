ee4ad05c486e2d97be5a9e0e19fff8cf30ddab0a
Quint Guvernator <quint@guvernator.net>
2024-04-07 20:52:12 +0200

    fix logs

--- OuterWildsAtHome.cs
+++ OuterWildsAtHome.cs
@@ -6,21 +6,25 @@ namespace OuterWildsAtHome
 {
     public class OuterWildsAtHome : ModBehaviour
     {
+        private void Awake() {}
+
         private void Start()
         {
-            if (ModHelper.Interaction.ModExists("MegaPiggy.OWRichPresence"))
+            ModHelper.Console.WriteLine("Checking for MegaPiggy.OWRichPresence...");
+            if (!ModHelper.Interaction.ModExists("MegaPiggy.OWRichPresence"))
             {
                 ModHelper.Console.WriteLine("Please install or update the Discord Rich Presence mod: https://outerwildsmods.com/mods/discordrichpresence owmods://install-mod/MegaPiggy.OWRichPresence", MessageType.Error);
                 return;
             }
 
+            ModHelper.Console.WriteLine("Registering handler...");
             var richPresence = ModHelper.Interaction.TryGetModApi<IRichPresenceAPI>("MegaPiggy.OWRichPresence");
             richPresence.RegisterHandler(StatusHandler);
-            ModHelper.Console.WriteLine("Registered handler", MessageType.Debug);
+            ModHelper.Console.WriteLine("Registered handler");
         }
 
         private void StatusHandler(string details, string largeImageKey, string largeImageText) =>
-            ModHelper.Console.WriteLine($"details: {details}; largeImageKey: {largeImageKey}; largeImageText: {largeImageText};", MessageType.Debug);
+            ModHelper.Console.WriteLine($"details: {details}; largeImageKey: {largeImageKey}; largeImageText: {largeImageText};");
     }
 
 }
