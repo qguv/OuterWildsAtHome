53e0916b9e54e0feb57a1d6d7d9a51844b92e838
Quint Guvernator <quint@guvernator.net>
2024-04-07 21:59:24 +0200

    push to hass api

--- OuterWildsAtHome.cs
+++ OuterWildsAtHome.cs
@@ -1,4 +1,7 @@
-﻿using OWML.Common;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using OWML.Common;
 using OWML.ModHelper;
 using OWRichPresence.API;
 
@@ -6,7 +9,7 @@ namespace OuterWildsAtHome
 {
     public class OuterWildsAtHome : ModBehaviour
     {
-        private void Awake() {}
+        private static readonly HttpClient client = new();
 
         private void Start()
         {
@@ -21,10 +24,37 @@ namespace OuterWildsAtHome
             var richPresence = ModHelper.Interaction.TryGetModApi<IRichPresenceAPI>("MegaPiggy.OWRichPresence");
             richPresence.RegisterHandler(StatusHandler);
             ModHelper.Console.WriteLine("Registered handler");
+
+            Configure(ModHelper.Config);
+        }
+        public override void Configure(IModConfig config)
+        {
+            ModHelper.Console.WriteLine("SWEETPOTATO configure called");
+
+            var url = ModHelper.Config.GetSettingsValue<string>("Home Assistant URL");
+            var token = ModHelper.Config.GetSettingsValue<string>("Home Assistant long-lived access token");
+            var sensor = ModHelper.Config.GetSettingsValue<string>("Home Assistant entity ID");
+
+            if (!url.EndsWith("/"))
+            {
+                url += "/";
+            }
+            client.BaseAddress = new Uri($"{url}api/states/{sensor}");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
         }
 
-        private void StatusHandler(string details, string largeImageKey, string largeImageText) =>
+        private void StatusHandler(string details, string largeImageKey, string largeImageText)
+        {
             ModHelper.Console.WriteLine($"details: {details}; largeImageKey: {largeImageKey}; largeImageText: {largeImageText};");
+            client.PostAsJsonAsync("/", new
+            {
+                state = largeImageText,
+                attributes = new
+                {
+                    activity = details,
+                    icon = largeImageKey,
+                }
+            });
+        }
     }
-
 }
